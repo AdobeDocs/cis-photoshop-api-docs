8141c1f7127003355460a776795e8474b93dc536
Sudipta Khound <khound@adobe.com>
2023-06-12 11:29:20 -0700

    updated the code

--- sample-code/storage-app/googledrive/presignedURLs.js
+++ sample-code/storage-app/googledrive/presignedURLs.js
@@ -4,53 +4,45 @@ Demonstration to:create public URL of a file.
 
 Instructions to run
 npm install
-./presignedURLs.js
+node presignedURLs.js
 
 service-account.json is a generated file by google drive autorizarion. We have attached a sample example
+Photoshoshop APIs don't support redirect . So you need to use redirected url (line 42)
 */
 const { google } = require('googleapis');
-const path = require('path');
 const fs = require('fs');
-const request1 = require('request')
 const followRedirect = require('follow-redirect-url');
 
-const KEYFILEPATH = path.join(`./service-account.json`);
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
+const SERVICE_ACCOUNT_FILE = './service-account.json';
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: KEYFILEPATH,
-  scopes: SCOPES,
-});
+async function getFileUrl(fileId) {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: SERVICE_ACCOUNT_FILE,
+    scopes: SCOPES,
+  });
 
-const drive = google.drive({
-  version: 'v3',
-  auth,
-});
+  const drive = google.drive({ version: 'v3', auth });
 
-async function generatePresignedUrl() {
   try {
-    const fileId = 'YOUR FILE ID';
-    await drive.permissions.create({
-      fileId: fileId,
-      requestBody: {
-        role: 'reader',
-        type: 'anyone',
-      },
-    });
-    const result = await drive.files.get({
-      fileId: fileId,
-      fields: 'webViewLink, webContentLink',
-    });
-    followRedirect.startFollowing(result.data.webContentLink).then(urls => {
-        //Printing out just to give the details
-        console.log(urls);
-        //Copy this url and put it in Photoshop API payload as input asset
-        console.log(urls[1].url);
-      }).catch(error => {
-      console.log(error)
-    })
+    const res = await drive.files.get({ fileId, fields: 'webContentLink' });
+    return res.data.webContentLink;
   } catch (error) {
-    console.log(error.message);
+    console.error('An error occurred:', error.message);
+    return null;
   }
 }
-generatePresignedUrl();
+
+async function main() {
+  const fileId = 'your-file-id-goes-here';
+  const fileUrl = await getFileUrl(fileId);
+
+  if (fileUrl) {
+    const redirectlink = await followRedirect.startFollowing(fileUrl)
+    console.log('The URL for the file is:', redirectlink[1].url);
+  } else {
+    console.log('Failed to retrieve the file URL.');
+  }
+}
+
+main();
